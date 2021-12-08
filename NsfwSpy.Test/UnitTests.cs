1ac01c0a2c8a6dde042be7e15cfc17ae6d2ac3bb
Jamie Matthews <jamiep.matthews@outlook.com>
2021-12-08 15:47:49 +0000

    Expand testing with other video file formats

--- NsfwSpy.Test/UnitTests.cs
+++ NsfwSpy.Test/UnitTests.cs
@@ -225,77 +225,93 @@ namespace NsfwSpyNS.Test
             Assert.False(result.IsNsfw);
         }
 
-        [Fact]
-        public void ClassifyMp4ByteArray_ValidByteArray()
+        [Theory]
+        [InlineData("bikini.avi")]
+        [InlineData("bikini.mkv")]
+        [InlineData("bikini.mp4")]
+        [InlineData("bikini.webm")]
+        public void ClassifyVideoByteArray_ValidByteArray(string filename)
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, @"Assets\bikini.mp4");
+            var filePath = Path.Combine(AppContext.BaseDirectory, $@"Assets\{filename}");
             var imageBytes = File.ReadAllBytes(filePath);
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(imageBytes);
+            var result = nsfwSpy.ClassifyVideo(imageBytes);
 
             Assert.Equal(181, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
-        [Fact]
-        public void ClassifyMp4FilePath_ValidFilePath()
+        [Theory]
+        [InlineData("bikini.avi")]
+        [InlineData("bikini.mkv")]
+        [InlineData("bikini.mp4")]
+        [InlineData("bikini.webm")]
+        public void ClassifyVideoFilePath_ValidFilePath(string filename)
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, @"Assets\bikini.mp4");
+            var filePath = Path.Combine(AppContext.BaseDirectory, $@"Assets\{filename}");
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(filePath);
+            var result = nsfwSpy.ClassifyVideo(filePath);
 
             Assert.Equal(181, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
-        [Fact]
-        public void ClassifyMp4FilePath_ClassifyEvery2ndFrame()
+        [Theory]
+        [InlineData("bikini.avi")]
+        [InlineData("bikini.mkv")]
+        [InlineData("bikini.mp4")]
+        [InlineData("bikini.webm")]
+        public void ClassifyVideoFilePath_ClassifyEvery2ndFrame(string filename)
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, @"Assets\bikini.mp4");
+            var filePath = Path.Combine(AppContext.BaseDirectory, $@"Assets\{filename}");
             var videoOptions = new VideoOptions
             {
                 ClassifyEveryNthFrame = 2
             };
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(filePath, videoOptions);
+            var result = nsfwSpy.ClassifyVideo(filePath, videoOptions);
 
             Assert.Equal(91, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
-        [Fact]
-        public void ClassifyMp4FilePath_EndEarlyOnNsfw()
+        [Theory]
+        [InlineData("bikini.avi")]
+        [InlineData("bikini.mkv")]
+        [InlineData("bikini.mp4")]
+        [InlineData("bikini.webm")]
+        public void ClassifyVideoFilePath_EndEarlyOnNsfw(string filename)
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, @"Assets\bikini.mp4");
+            var filePath = Path.Combine(AppContext.BaseDirectory, $@"Assets\{filename}");
             var videoOptions = new VideoOptions
             {
                 EarlyStopOnNsfw = true
             };
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(filePath, videoOptions: videoOptions);
+            var result = nsfwSpy.ClassifyVideo(filePath, videoOptions: videoOptions);
 
             Assert.True(result.IsNsfw);
             Assert.True(result.Frames.Count < 181); // This video has 181 frames
         }
 
         [Fact]
-        public void ClassifyMp4Uri_ValidUri()
+        public void ClassifyVideoUri_ValidUri()
         {
             var uri = new Uri("https://i.imgur.com/MjTH5ZS.mp4");
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(uri);
+            var result = nsfwSpy.ClassifyVideo(uri);
 
             Assert.Equal(120, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
         [Fact]
-        public void ClassifyMp4Uri_ClassifyEvery2ndFrame()
+        public void ClassifyVideoUri_ClassifyEvery2ndFrame()
         {
             var uri = new Uri("https://i.imgur.com/MjTH5ZS.mp4");
             var videoOptions = new VideoOptions
@@ -304,14 +320,14 @@ namespace NsfwSpyNS.Test
             };
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(uri, videoOptions: videoOptions);
+            var result = nsfwSpy.ClassifyVideo(uri, videoOptions: videoOptions);
 
             Assert.Equal(60, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
         [Fact]
-        public void ClassifyMp4Uri_EndEarlyOnNsfw()
+        public void ClassifyVideoUri_EndEarlyOnNsfw()
         {
             var uri = new Uri("https://i.imgur.com/MjTH5ZS.mp4");
             var videoOptions = new VideoOptions
@@ -320,31 +336,35 @@ namespace NsfwSpyNS.Test
             };
 
             var nsfwSpy = new NsfwSpy();
-            var result = nsfwSpy.ClassifyMp4(uri, videoOptions: videoOptions);
+            var result = nsfwSpy.ClassifyVideo(uri, videoOptions: videoOptions);
 
             Assert.True(result.IsNsfw);
             Assert.True(result.Frames.Count < 120); // This video has 120 frames
         }
 
-        [Fact]
-        public async Task ClassifyMp4FilePathAsync_ValidFilePath()
+        [Theory]
+        [InlineData("bikini.avi")]
+        [InlineData("bikini.mkv")]
+        [InlineData("bikini.mp4")]
+        [InlineData("bikini.webm")]
+        public async Task ClassifyVideoFilePathAsync_ValidFilePath(string filename)
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, @"Assets\bikini.mp4");
+            var filePath = Path.Combine(AppContext.BaseDirectory, $@"Assets\{filename}");
 
             var nsfwSpy = new NsfwSpy();
-            var result = await nsfwSpy.ClassifyMp4Async(filePath);
+            var result = await nsfwSpy.ClassifyVideoAsync(filePath);
 
             Assert.Equal(181, result.Frames.Count);
             Assert.True(result.IsNsfw);
         }
 
         [Fact]
-        public async Task ClassifyMp4UriAsync_ValidUri()
+        public async Task ClassifyVideoUriAsync_ValidUri()
         {
             var uri = new Uri("https://i.imgur.com/MjTH5ZS.mp4");
 
             var nsfwSpy = new NsfwSpy();
-            var result = await nsfwSpy.ClassifyMp4Async(uri);
+            var result = await nsfwSpy.ClassifyVideoAsync(uri);
 
             Assert.Equal(120, result.Frames.Count);
             Assert.True(result.IsNsfw);
